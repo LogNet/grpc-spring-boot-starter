0d489e891dda28495d61d0907cd4ef3a1b0208a6
jvmlet <furer.alexander@gmail.com>
2016-03-13 10:34:34 +0200

    adding actuators to test case, so grpc and tomcat run side by side

--- grpc-spring-boot-starter-demo/src/test/java/org/lognet/springboot/grpc/GreeterAppTest.java
+++ grpc-spring-boot-starter-demo/src/test/java/org/lognet/springboot/grpc/GreeterAppTest.java
@@ -5,6 +5,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.examples.GreeterGrpc;
 import io.grpc.examples.GreeterOuterClass;
+import io.netty.handler.codec.http.HttpMethod;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,17 +13,24 @@ import org.junit.runner.RunWith;
 import org.lognet.springboot.grpc.demo.GreeterApp;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by alexf on 28-Jan-16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(GreeterApp.class)
-@IntegrationTest
+@WebIntegrationTest
 
 public class GreeterAppTest {
     private ManagedChannel channel;
@@ -46,4 +54,13 @@ public class GreeterAppTest {
         final String replay = greeterFutureStub.sayHello(helloRequest).get().getMessage();
         System.out.println(replay);
     }
+
+    @Test
+    public  void actuatorTest () throws ExecutionException, InterruptedException {
+        final TestRestTemplate template = new TestRestTemplate();
+
+        ResponseEntity<String> response = template.getForEntity("http://localhost:8080/env",String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
 }
