baba7fa837092cff12a8db6abacfa27b3f486f5f
Alexander Furer <alexander.furer@microfocus.com>
2020-12-30 10:43:00 +0200

    conditional

--- grpc-spring-boot-starter/src/main/java/org/lognet/springboot/grpc/autoconfigure/security/GrpcSecurityEnablerConfiguration.java
+++ grpc-spring-boot-starter/src/main/java/org/lognet/springboot/grpc/autoconfigure/security/GrpcSecurityEnablerConfiguration.java
@@ -1,13 +1,10 @@
 package org.lognet.springboot.grpc.autoconfigure.security;
 
 import org.lognet.springboot.grpc.security.EnableGrpcSecurity;
-import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@ConditionalOnBean(GrpcSecurityConfigurerAdapter.class)
 @EnableGrpcSecurity
 public class GrpcSecurityEnablerConfiguration {
 
--- grpc-spring-boot-starter/src/main/java/org/lognet/springboot/grpc/autoconfigure/security/SecurityAutoConfiguration.java
+++ grpc-spring-boot-starter/src/main/java/org/lognet/springboot/grpc/autoconfigure/security/SecurityAutoConfiguration.java
@@ -1,9 +1,12 @@
 package org.lognet.springboot.grpc.autoconfigure.security;
 
+import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+@ConditionalOnBean(GrpcSecurityConfigurerAdapter.class)
 @Import(GrpcSecurityEnablerConfiguration.class)
 public class SecurityAutoConfiguration {
 }
