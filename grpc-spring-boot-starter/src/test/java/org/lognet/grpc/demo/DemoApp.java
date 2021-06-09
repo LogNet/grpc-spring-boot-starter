f0708d3db5e99ca62de3758c8fc022a602e75352
Alexander Furer <alexander.furer@microfocus.com>
2021-06-09 16:15:24 +0300

    progress with spring-native support

--- grpc-spring-boot-starter/src/test/java/org/lognet/grpc/demo/DemoApp.java
+++ grpc-spring-boot-starter/src/test/java/org/lognet/grpc/demo/DemoApp.java
@@ -1,35 +1,31 @@
-package org.lognet.springboot.grpc._native.demo;
+package org.lognet.grpc.demo;
 
 import io.grpc.examples.GreeterGrpc;
 import io.grpc.examples.GreeterOuterClass;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-
 @SpringBootApplication
-public class NativeDemoApp {
-
+public class DemoApp {
     @GRpcService
-    static class HelloService extends GreeterGrpc.GreeterImplBase{
+    public static class HelloService extends GreeterGrpc.GreeterImplBase{
         @Override
         public void sayHello(GreeterOuterClass.HelloRequest request, StreamObserver<GreeterOuterClass.HelloReply> responseObserver) {
             responseObserver.onNext(GreeterOuterClass.HelloReply.newBuilder()
-                    .setMessage(String.format("Hello, %s", request.getName()))
+                    .setMessage(String.format("Hello,%s", request.getName()))
                     .build());
             responseObserver.onCompleted();
         }
     }
 
     public static void main(String[] args) {
-        final ConfigurableApplicationContext applicationContext = SpringApplication.run(NativeDemoApp.class, args);
-        final Boolean autoStop = applicationContext.getEnvironment().getProperty("auto-stop", Boolean.class, false);
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApp.class, args);
+        final Boolean autoStop = applicationContext.getEnvironment().getProperty("auto-close", Boolean.class, false);
         if (autoStop) {
             applicationContext.close();
         }
     }
 }
-
