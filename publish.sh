4a1993d956f6278d83fed7b83ce8ba18880d47db
Alexander Furer <alexander.furer@microfocus.com>
2021-06-17 16:03:14 +0300

    publish gradle plugin to sonatype repo

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ${TRAVIS_PULL_REQUEST} == 'false' ]] && [[ ${TRAVIS_BRANCH} == 'master' ]] && [[ ${TRAVIS_TAG} == '' ]]; then
- ./gradlew -S publishToSonatype  closeAndReleaseRepository
+ #./gradlew -S publishToSonatype  closeAndReleaseRepository
  ./gradlew -S -p grpc-spring-boot-starter-gradle-plugin build publishToSonatype   closeAndReleaseRepository
 # ./gradlew -S -p grpc-spring-boot-starter-gradle-plugin build publishPlugins
 fi
