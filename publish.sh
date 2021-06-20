ab5c1e3f0b7784aeb55ebffc6912062845d7df94
Alexander Furer <alexander.furer@microfocus.com>
2021-06-20 09:15:42 +0300

    next dev version

--- publish.sh
+++ publish.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 if [[ ${TRAVIS_PULL_REQUEST} == 'false' ]] && [[ ${TRAVIS_BRANCH} == 'master' ]] && [[ ${TRAVIS_TAG} == '' ]]; then
- #./gradlew -S publishToSonatype  closeAndReleaseRepository
- ll
- ll ./grpc-spring-boot-starter-gradle-plugin
- ./gradlew -S -p grpc-spring-boot-starter-gradle-plugin build publishToSonatype   closeAndReleaseRepository
-# ./gradlew -S -p grpc-spring-boot-starter-gradle-plugin build publishPlugins
+ ./gradlew -S publishToSonatype  closeAndReleaseRepository
+ ./gradlew -S -p grpc-spring-boot-starter-gradle-plugin build publishToSonatype   closeAndReleaseRepository publishPlugins
 fi
